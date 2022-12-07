e048b97dee761f62417a8d02b023ffb215bf38ac
Nagashreehr <nagashreehr2016@gmail.com>
2022-12-07 11:41:57 +0530

    third commit

---
+++ src/app/materials/materials.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatBadgeModule
+  ],
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatBadgeModule
+
+  ]
+})
+export class MaterialsModule { }
